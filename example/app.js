d7e20077cb210cf124fbcb502df676decd8d958e
youyudehexie <405574395@qq.com>
2013-06-20 20:40:00 +0800

    add some function

--- example/app.js
+++ example/app.js
@@ -1,5 +1,6 @@
 var getCache = require('../lib').getCache;
 var cache = require('../lib');
+var recoverCache = require('../lib').recoverCache;
 
 cache.init();
 
@@ -9,3 +10,12 @@ getCache('aa', function(cb){
 },function(err, data){
     console.log(data);
 });
+
+recoverCache('test*', 'get', function(datas, cb){
+    datas.forEach(function(data){
+	console.log('data: ' + data);
+    });
+    cb(null, 'abc');
+},function(err){
+    console.log(err);
+})
